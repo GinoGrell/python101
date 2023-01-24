4a9232619df16cf7b93e5df1a0c129118a5b4df7
GinoGrell <120664990+GinoGrell@users.noreply.github.com>
2023-01-24 12:00:37 +0100

    Update write_and_read_file.py

--- write_and_read_file.py
+++ write_and_read_file.py
@@ -1,7 +1,11 @@
-my_file = open("my_file.txt", "w")
+with open("my_file.txt", "w") as file:
+    file.write("Hallo")    # so ist es übersichtlicher und die warscheinlichkeit die datei versehentlich zu überschreiben ist geringer,
+#                            da sie nicht nur in einer loop ist sondern auch weil sie automatisch geschlossen wird.
+
+'''my_file = open("my_file.txt", "w")
 
 my_file.write("Hallo")
-my_file.close()
+my_file.close()'''
 
 read_file = open("my_file.txt")
 for line in read_file.readlines():
